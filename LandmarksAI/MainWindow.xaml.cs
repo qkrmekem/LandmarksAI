fc2deed4fa4f3f210c7b45a1fbc245d1162b3ef8
qkrmekem <magicofclown@naver.com>
2024-07-10 15:39:38 +0900

    Add project files.

---
+++ LandmarksAI/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using LandmarksAI.Classes;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LandmarksAI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new();
+            // 파일 유형을 제한하는 필터
+            // "사용자에게 보일 내용|파일 유형"
+            dialog.Filter = "Image Files (*.png; *.jpg)|*.png;*.jpg;*.jpeg|All Files (*.*)|*.*";
+            // 파일 업로드 다이얼로그의 기본 경로를 설정
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if (dialog.ShowDialog() == true)
+            {
+                string fileName = dialog.FileName;
+                selectedImage.Source = new BitmapImage(new Uri(fileName));
+
+                MakePredictionAsync(fileName);
+            }
+        }
+
+        private async void MakePredictionAsync(string fileName)
+        {
+            string url = "https://japaneast.api.cognitive.microsoft.com/customvision/v3.0/Prediction/8319148f-5626-42a2-9680-81cc48e86547/classify/iterations/Iteration1/image";
+            string prediction_key = "4ce0ca47d7db43c19b0f761cb1374a9e";
+            string content_type = "application/octet-stream";
+            var file = File.ReadAllBytes(fileName);
+
+            using (HttpClient client = new())
+            {
+                client.DefaultRequestHeaders.Add("Prediction-key", prediction_key);
+
+                using (var content = new ByteArrayContent(file))
+                {
+                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(content_type);
+                    var response = await client.PostAsync(url, content);
+
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    List<Prediction> predictions = (JsonConvert.DeserializeObject<CustomVision>(responseString)).Predictions;
+                    predictionsListView.ItemsSource = predictions;
+                }
+            }
+        }
+    }
+}
